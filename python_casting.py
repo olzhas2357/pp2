cbf30c0730d9b056750802970047b6ddbc3617c9
Olzhas <olzhas.koshkarbay@gmail.com>
2023-01-24 19:56:11 +0600

    code

---
+++ python_casting.py
@@ -0,0 +1,13 @@
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
