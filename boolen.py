ea69e34a8d0e46e3fb6ae92c750b0eb761063a46
Olzhas <olzhas.koshkarbay@gmail.com>
2023-01-24 20:12:39 +0600

    letter

---
+++ boolen.py
@@ -0,0 +1,20 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
