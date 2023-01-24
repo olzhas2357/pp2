5e64f98858d5244c383f37f5ad99f52eb013f7fc
Olzhas <olzhas.koshkarbay@gmail.com>
2023-01-24 19:17:37 +0600

    olzhas

---
+++ python_syntax.py
@@ -0,0 +1,13 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
