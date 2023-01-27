279cd47ec645166c60706dc66ee1160c77a93eba
Olzhas <olzhas.koshkarbay@gmail.com>
2023-01-27 18:17:37 +0600

    the first commit

---
+++ tsis2/array.py
@@ -0,0 +1,29 @@
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+
+cars = ["Ford", "Volvo", "BMW"]
+x = cars[0]
+print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars[0] = "Toyota"
+print(cars)
+
+
+cars = ["Ford", "Volvo", "BMW"]
+x = len(cars)
+print(x)
+
+
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars:
+  print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.append("Honda")
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.pop(1)
+print(cars)
