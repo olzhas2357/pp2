04b4443b422b8dd63d35e90ff04532270420fdf3
olzhas2357 <122978915+olzhas2357@users.noreply.github.com>
2023-01-27 17:31:12 +0600

    Add files via upload

---
+++ tsis1/data_types.py
@@ -0,0 +1,55 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+
+x = 20
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+
+x = 20.5
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+
+x = 1j
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+
+x = ("apple", "banana", "cherry")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
