702e4241a1b20c10dd1fc7e2ca447566a98da4fe
Olzhas <olzhas.koshkarbay@gmail.com>
2023-02-05 18:20:43 +0600

    the first commit

--- function1 (12).py
+++
@@ -1,8 +0,0 @@
-# Write a function that accepts string from user, return a sentence with the words reversed. We are ready -> ready are We
-def reversed_let(s):
-    l = s.split()
-    for num in reversed(l):
-        print(num, end = " ")
-if __name__=="__main__":
-    s = input()
-    reversed_let(s)
--- function_1(15).py
+++
@@ -1,5 +0,0 @@
-def string(g):
-    ounces = n * 28.345
-    return ounces
-n = float(input())
-print(string(n))
